c70ceac69c8853346c95f011893e5d925fe8bbb7
Allan Wilson <allan.wilson@vaisala.com>
2017-09-01 16:15:35 +0300

    Initial commit

---
+++ src/com/allanwilson/Car.java
@@ -0,0 +1,15 @@
+package com.allanwilson;
+
+/**
+ * Created by awil on 2017-09-01.
+ */
+public class Car extends Vehicle {
+    private int doors;
+    private int engineCapacity;
+
+    public Car (String name, int doors, int engineCapacity) {
+        super(name);
+        this.doors = doors;
+        this.engineCapacity = engineCapacity;
+    }
+}
---
+++ src/com/allanwilson/Vehicle.java
@@ -0,0 +1,12 @@
+package com.allanwilson;
+
+/**
+ * Created by awil on 2017-09-01.
+ */
+public class Vehicle {
+    private String name;
+
+    public Vehicle (String name) {
+        this.name = name;
+    }
+}
